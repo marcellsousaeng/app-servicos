b2f708e673d5fe89446bc077dba149fcbd710754
Marcell Barbosa <marcellsousa.eng@gmail.com>
2026-05-08 09:45:11 -0300

    Correção faturamento, PDF e visualização de fotos

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://nsxrzwsofqroebpbzcxn.supabase.co'
-const supabaseKey = 
-'sb_publishable_8ITIdbVjKEwYhE1mbnlglQ_LKS0rz4f'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
